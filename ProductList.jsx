dd8045aef3c8ed25aff5864a2218d47d734ff8b1
vietlam2005 <dp1.1a2.lam@gmail.com>
2026-07-23 11:05:53 +0700

    Update ProductList.jsx

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import './ProductList.css';
 
+function ProductList() {
+  return (
+    <div className="product-grid">
+      <h2>Featured Houseplants</h2>
+      <p>Explore our wide collection of beautiful plants for your home and office.</p>
+    </div>
+  );
+}
+
+export default ProductList;
